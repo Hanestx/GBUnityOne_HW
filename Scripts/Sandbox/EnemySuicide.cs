0c5df07bcb48b6a5aad0d1975ef3297d8b06fd64
Hanest <Hanestx@ya.ru>
2020-07-23 12:43:31 +0300

    Папка Scripts продублирована в корень

---
+++ Scripts/Sandbox/EnemySuicide.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace ApoProject
+{
+    public class EnemySuicide : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] GameObject _enemySuicide;
+        [SerializeField] Transform _enemyPos;
+
+        #endregion
+
+
+        #region OnTriggers
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Destroy(gameObject.GetComponent<BoxCollider>());
+                CreateEnemyBomber();
+            }
+        }
+
+        #endregion
+
+
+        #region MyMethods
+
+        private void CreateEnemyBomber()
+        {
+            Instantiate(_enemySuicide, _enemyPos.position, _enemyPos.rotation);
+            Invoke("CreateEnemyBomber", 5f);
+        }
+
+        #endregion
+    }
+}
---
+++ Scripts/Sandbox/TriggerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+namespace ApoProject 
+{
+    public class TriggerController : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private bool _isKey;
+
+        #endregion
+
+
+        #region OnTriggers
+
+        private void OnTriggerStay(Collider other) 
+        {
+            if (other.CompareTag("Door") && _isKey)
+	    	{
+	    		if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Door door = other.GetComponent<Door>();
+                    door.IsOpen = !door.IsOpen;
+                }
+	    	}
+        }
+
+        private void OnTriggerEnter(Collider other) 
+        {
+            if (other.CompareTag("Key"))
+	    	{
+	    		Destroy(other.gameObject);
+	    		_isKey = true;
+	    	}
+        }
+
+        #endregion
+    }
+
+}

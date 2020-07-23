05b7cbe01c647da41967a2ad78400ebb4da9355b
Hanest <Hanestx@ya.ru>
2020-07-23 12:40:40 +0300

    Собрана тестовая зона
    Добавлено (Sandbox):
    
    - Камера за персонажем (third-person)
    - Управление персонажем (Базовое передвижение, поворот, прыжок)
    - Комнаты для тестов (Ключ + Вражеская Турель, Установка турели и мин, Кнопка открытия двери, Граната)
    - Установка турели
    - Автоматическая стрельба турели по врагам
    - ИИ врагов (Слежение за персонажем)
    - Мина
    - Ключ от двери
    - NavMesh
    - Подбор предметов
    - Добавлены физика предметов
    - Добавлены hp и damage врагам.
    - Кол-во предметов и Help выведены в TextMesh

---
+++ Maximov_ApoProject/Assets/Scripts/EnemyHpUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+namespace ApoProject
+{
+    public class EnemyHpUI : MonoBehaviour
+    {
+        public string HpShow
+        {
+            set { GetComponent<TextMesh>().text = value; }
+        }
+    }
+}
---
+++ Maximov_ApoProject/Assets/Scripts/Sandbox/ButtonDoor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+namespace ApoProject
+{
+    public class ButtonDoor : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] GameObject _wall;
+
+        #endregion
+
+
+        #region OnTriggers
+
+        private void OnTriggerStay(Collider other) 
+        {
+            if (other.gameObject.CompareTag("Box"))
+            {
+                Destroy(_wall.gameObject);
+            }
+        }
+
+        #endregion
+    }
+}
